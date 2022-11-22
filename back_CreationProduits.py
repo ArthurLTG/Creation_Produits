27c0dfffcc639eab9103f78fb408a236344b000e
ArthurLTG <109527679+ArthurLTG@users.noreply.github.com>
2022-11-22 16:36:59 +0100

    Update back_CreationProduits.py

--- back_CreationProduits.py
+++ back_CreationProduits.py
@@ -5,12 +5,13 @@ import certifi
 import urllib3
 import gspread 
 from oauth2client.service_account import ServiceAccountCredentials 
+from google.oauth2 import service_account
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets', 
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"] 
 
 #creds = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/arthu/Desktop/VSC/Creds.json", scope) 
-creds = ServiceAccountCredentials.from_service_account_info(st.secrets["gcp_service_account"], scopes = scope) 
+creds = service_account.from_service_account_info(st.secrets["gcp_service_account"], scopes = scope) 
 client = gspread.authorize(creds) 
 
 #df = pd.read_csv(r'shy.csv')
