454fe39d9c72dde223930aee731f36c1e89928c3
ArthurLTG <109527679+ArthurLTG@users.noreply.github.com>
2022-11-22 16:50:29 +0100

    Update back_CreationProduits.py

--- back_CreationProduits.py
+++ back_CreationProduits.py
@@ -11,7 +11,7 @@ scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/au
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"] 
 
 #creds = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/arthu/Desktop/VSC/Creds.json", scope) 
-creds = service_account.from_service_account_info(st.secrets["gcp_service_account"], scopes = scope) 
+creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes = scope) 
 client = gspread.authorize(creds) 
 
 #df = pd.read_csv(r'shy.csv')
