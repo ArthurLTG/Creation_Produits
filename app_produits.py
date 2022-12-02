1550359b5c4e26c20809941bf254b82da9d3bbf6
ArthurLTG <109527679+ArthurLTG@users.noreply.github.com>
2022-12-02 13:59:35 +0100

    Update app_produits.py

--- app_produits.py
+++ app_produits.py
@@ -59,31 +59,6 @@ with col5:
     st.button(label="Création", key="boutonRun", on_click=run)
 st.markdown("---")
 
-col11, col22 = st.columns(2)
-if catalogue == 'Shopify':
-    
-    with col11:
-       
-        with st.expander(label="Colonnes FIELDS", expanded=False):
-            st.markdown("- 'Template Suffix'")
-            st.markdown("- 'Tags'  : à compléter : Rayon, Saison, Famille, SsFamille, Matiere, Couleur Mere, RefCo")
-            st.markdown("- 'Image Src' : Uniquement les packshots sont intégrés depuis les Files Shopify donc nomenclature des photo cruciale")
-    
-    with col22:
-      
-        with st.expander(label="Colonnes METAFIELDS", expanded=False):
-            
-            st.markdown("- Metafield: sf_product_infos.style [string] = Keywords des filtres de grille (best practices à definir)")
-            st.markdown("- Metafield: sf_product_infos.shape [string] = Keyword de forme du vetement")
-            st.markdown("- Metafield: sf_product_infos.category [string] = Keywords des filtres de grille : lors de la création ajout auto Famille, SousFamille (à compléter)")
-            st.markdown("- Metafield: sf_product_infos.thickness [string] = Nombre de fils ex: '2 fils'")
-            st.markdown("- Metafield: sf_product_infos.thread_number [integer] = Nombre de fils ex: '2'")
-            st.markdown("- Metafield: sf_product_infos.thread_number_description [string] = Vide")
-            st.markdown("- Metafield: sf_product_infos.length [string] = Vide")
-            st.markdown("- Metafield: sf_product_infos.sleeve [string] = Vide")
-            st.markdown("- Metafield: sf_product_tabs.infos_size [string] = Toggle: Texte à récupérer/adapter ⚠️")
-            st.markdown("- Metafield: sf_product_tabs.infos_compo [string] = Toggle: Texte à récupérer/adapter aupres du CRM ⚠️")
-            st.markdown("- Metafield: sf_product_tabs.infos_shipping [string] = Vide")
 
  
 
